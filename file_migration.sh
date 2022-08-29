30e9ad0cd4a016cd799ba6bf457b1ab8a55235d0
pinkypsy <pinkypsy@gmail.com>
2022-08-29 22:13:41 +0300

    migration script modified

--- file_migration.sh
+++ file_migration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ -z "$SOURCE_PATH" ]; then read -r -p "Enter source path to the file or directory: " SOURCE_PATH
+if [ -z "$SOURCE_PATH" ]; then
+  read -r -p "Enter source path to the file or directory: " SOURCE_PATH
 fi
 echo ">Source: $SOURCE_PATH"
 
@@ -13,22 +14,26 @@ else
 fi
 echo ">Number of files to copy: $COUNT"
 
-if [ -z "$DESTINATION_PATH" ]; then read -r -p "Enter destination path (pattern: STORAGE_ACCOUNT_NAME:/CONTAINER_NAME/destination/path): " DESTINATION_PATH
+if [ -z "$DESTINATION_PATH" ]; then
+  read -r -p "Enter destination path (pattern: STORAGE_ACCOUNT_NAME:/CONTAINER_NAME/destination/path): " DESTINATION_PATH
 fi
 echo ">Destination: $DESTINATION_PATH"
 
+read -r -p "Would you like to replace files in the destination if files with the same name are detected? [y/n] " RESPONSE
+case "$RESPONSE" in
+[Nn]) IS_IGNORE_EXISTING=--ignore-existing ;;
+esac
+
 rclone copy -v "$SOURCE_PATH" "$DESTINATION_PATH" \
-  --transfers $COUNT --checkers $COUNT \
+  $IS_IGNORE_EXISTING --transfers $COUNT --checkers $COUNT \
   --azureblob-disable-checksum --stats 10s --stats-file-name-length 0 \
   --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 \
-  --azureblob-memory-pool-flush-time 60s \
-  --azureblob-upload-concurrency 256 \
-  --azureblob-chunk-size 32M \
-  --error-on-no-transfer \
-  --azureblob-memory-pool-use-mmap #?
+  --azureblob-memory-pool-flush-time 60s --azureblob-upload-concurrency 256 \
+  --azureblob-chunk-size 32M --azureblob-memory-pool-use-mmap \
+  --error-on-no-tranfer
 
 EXIT_CODE=$?
-echo EXIT_CODE
+echo "Exit code: $EXIT_CODE"
 
 if [ $EXIT_CODE -eq 0 ]; then
   echo "Operation successful"
@@ -53,11 +58,3 @@ elif [ $EXIT_CODE -eq 9 ]; then
 fi
 
 #for access logs  --azureblob-access-tier archive
-
-#3 - Directory not found
-#4 - File not found
-#5 - Temporary error (one that more retries might fix) (Retry errors)
-#6 - Less serious errors (like 461 errors from dropbox) (NoRetry errors)
-#7 - Fatal error (one that more retries won't fix, like account suspended) (Fatal errors)
-#8 - Transfer exceeded - limit set by --max-transfer reached
-#9 - Operation successful, but no files transferred
